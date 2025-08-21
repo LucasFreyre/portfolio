5a909038ac631eafe07d95fc14dda9645865b9b9
LucasFreyre <lucasfreiremoreira1@gmail.com>
2025-08-21 11:36:49 -0300

    Adicionei o projeto do lado do contato

--- darkMode.js
+++ darkMode.js
@@ -1,16 +1,22 @@
 const body = document.body;
 const btn = document.querySelector('.sun-btn');
+const sun = document.querySelector(".sun")
+const moon = document.querySelector(".moon")
 
 let darkMode = localStorage.getItem('darkmode');
 
 function enableDarkMode() {
     body.classList.add('darkmode');
     localStorage.setItem('darkmode', 'active');
+    sun.style.display = 'none';
+    moon.style.display = 'block';
 }
 
 function disableDarkMode() {
     body.classList.remove('darkmode');
     localStorage.removeItem('darkmode');
+    sun.style.display = 'block';
+    moon.style.display = 'none';
 }
 
 if (darkMode === 'active') enableDarkMode();
